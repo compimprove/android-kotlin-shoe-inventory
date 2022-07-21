a830c1418b21eefb87880d9e0d38c503d94880be
dinhnt <compimprove@gmail.com>
2022-07-21 21:08:49 +0700

    third

---
+++ app/src/main/java/com/example/shoestore/screen/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.shoestore.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.shoestore.R
+import com.example.shoestore.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentWelcomeBinding>(
+            inflater,
+            R.layout.fragment_welcome,
+            container,
+            false
+        )
+        binding.toInstructionButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_instructionFragment))
+        return binding.root
+    }
+}
